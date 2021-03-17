e8bfe41748f50a6af20f38e766c0b062899412ba
pa1012 <phuonganht1012@gmail.com>
2021-03-17 15:51:50 +0700

    Added listing recipe

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 
 
 class UserManager(BaseUserManager):
