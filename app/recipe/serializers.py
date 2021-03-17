241834f97d4e1666626bdb970b2b6155f442776a
pa1012 <phuonganht1012@gmail.com>
2021-03-17 17:51:31 +0700

    Added tests for recipe api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -37,3 +37,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredients', 'tags',
                   'time_minutes', 'price', 'link')
         read_only_fields = ('id', )
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
